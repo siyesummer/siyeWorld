3c1bdecd56e2c1fc7e6465cc49406fa91d33e1b6
Lxq <2293147233@qq.com>
2020-04-12 13:14:08 +0800

    chore:根目录下src文件夹别名; 新增`request`,用于发送ajax请求

--- src/configs/pathAlias.js
+++ src/configs/pathAlias.js
@@ -3,7 +3,7 @@
  * 生成 webpack-dev-server 的 alias 配置
  */
 const path = require('path');
-const { packagesInfo } = require('./packageManifest');
+const { packagesInfo, host } = require('./packageManifest');
 
 const alias = {};
 
@@ -11,5 +11,7 @@ packagesInfo.forEach(pkg => {
   // 增加包下 'src/' 路径别名
   alias[`@${pkg.name}`] = path.join(pkg.path, 'src');
 });
+
+alias['@ROOT'] = path.join(host.path, 'src');
 
 module.exports = alias;
