eddd36454d4fb679db3fda614dc24d19b0a1bbf2
Lxq <2293147233@qq.com>
2020-09-12 23:05:02 +0800

    chore(siye-music): 添加自动发布插件`standard-version`

---
+++ packages/siye-music/.versionrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  // 标签前缀
+  "tag-prefix": "siye-music@",
+  // 生命周期执行配置(是否跳过)
+  "skip": {
+    "bump": false,
+    "changelog": false,
+    "commit": false,
+    "tag": false
+  },
+  // 生命周期脚本
+  "scripts": {
+    // 添加标签后执行
+    "posttag": "git push --tags origin master"
+  },
+  // 提交信息
+  "release-commit-message-format": "chore(release): siye-music@{{currentTag}}",
+  // 日志显示分类类型
+  "types": [{
+      "type": "feat",
+      "section": "Features"
+    },
+    {
+      "type": "fix",
+      "section": "Bug Fixes"
+    },
+    {
+      "type": "chore",
+      "hidden": true
+    },
+    {
+      "type": "docs",
+      "section": "Docs"
+    },
+    {
+      "type": "style",
+      "section": "Style"
+    },
+    {
+      "type": "refactor",
+      "section": "Refactor"
+    },
+    {
+      "type": "Revert",
+      "section": "Revert"
+    },
+    {
+      "type": "improvement",
+      "section": "Improvement"
+    }
+  ]
+}
