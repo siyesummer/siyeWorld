d8134702ccc18e08a454631bda38a8c143d71336
Lxq <2293147233@qq.com>
2023-02-28 10:16:26 +0800

    fix(easy-chat/serve.js): 链接源配置调整为`*`，允许任何ip访问，解决跨域问题

--- packages/easy-chat/server.js
+++ packages/easy-chat/server.js
@@ -2,16 +2,16 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const socketIo = require('socket.io');
 const logger = require('siye-core/src/utils/logger');
-const { SOCKET_ORIGIN, LISTENING_PORT, CONNECT_URL } = require('./config');
+const { LISTENING_PORT, CONNECT_URL } = require('./config');
 
 const io = socketIo(http, {
   cors: {
     /**
      * 从v3开始需要显式启用跨域
      * @see https://socket.io/docs/v3/handling-cors/
+     * @tip 调整为允许任何ip访问，解决跨域问题
      */
-    origin: [SOCKET_ORIGIN],
-
+    origin: '*',
   }
 });
 
