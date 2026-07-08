5016196dba108bb8e5d0539e9a21319ac7db83b9
siyesummer <2293147233@qq.com>
2026-07-08 18:19:08 +0800

    feat: 添加备案号展示

--- src/components/MyWorld.vue
+++ src/components/MyWorld.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
+  <div class="my-world">
     <SiyeMusic />
     <EasyChat />
+    <div class="record-number">{{ recordNumber }}</div>
   </div>
 </template>
 
@@ -14,9 +15,27 @@ export default {
   props: {
     value: String,
   },
+  data() {
+    return {
+      recordNumber: '\u95fdICP\u590717032186\u53f7-2',
+    };
+  },
   components: {
     SiyeMusic,
     EasyChat,
   },
 };
 </script>
+
+<style scoped>
+.my-world {
+  position: relative;
+}
+
+.record-number {
+  padding: 16px 0 24px;
+  color: #999;
+  font-size: 12px;
+  text-align: center;
+}
+</style>
